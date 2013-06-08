10b269634daf343415f94ef7d1505a069576db1c
Michael Reinhart <mreinhart88@gmail.com>
2013-06-08 15:26:38 -0400

    first commit. only made files and db

---
+++ nycsports/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nycsports::Application.config.secret_token = 'c01ce96c725167ae621f168ebcf6f9a75c7ba91985a8112e174536d1e8a4f41b41ef52d29064883d191ab29ecf300400709038f717ceb02be043591990d061a5'
